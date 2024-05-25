ec783d0c5b6bba5dab1d73e2f9c120da321355dd
priyarajanharshini <priyamaha0707@gmail.com>
2024-05-25 13:50:51 +0530

    topbar

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          "dark-green": '#448F30',
+          "light-green": '#DEFAD4',
+          "grey-green": '#92ab89',
+          "green-shaded": '#6cbd4f',
+          "very-light-green": '#84bf71',
+          "parrot-green": '#67FE33',
+          "remove-red": '#eb4034',
+          "card-green":'#F2FFED',
+          "gray-color": '#cccccc'
+        },
+        fontFamily: {
+          'roboto': ['"roboto"'],
+          'nunito': ['"nunito"']
+        },
+        boxShadow: {
+          '3xl': '#000000',
+        }
+      },
+    },
+    plugins: [],
+  }
+
+
