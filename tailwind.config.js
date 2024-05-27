dc66d02eb746344aef8119461672c14091409630
kavitha9894 <skavitha9894@gmail.com>
2024-05-27 12:13:07 +0530

    code merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,68 +1,4 @@
-// // /** @type {import('tailwindcss').Config} */
-// // module.exports = {
-// //     purge: ["./src//*.{js,jsx,ts,tsx}", "./public/index.html"],
-// //     content: ["./src//*.{html,js}"],
-// //     theme: {
-// //       extend: {
-// //         colors: {
-// //           "dark-green": '#448F30',
-// //           "light-green": '#DEFAD4',
-// //           "grey-green": '#92ab89',
-// //           "green-shaded": '#6cbd4f',
-// //           "very-light-green": '#84bf71',
-// //           "parrot-green": '#67FE33',
-// //           "remove-red": '#eb4034',
-// //           "card-green":'#F2FFED',
-// //           "gray-color": '#cccccc'
-// //         },
-// //         fontFamily: {
-// //           'roboto': ['"roboto"'],
-// //           'nunito': ['"nunito"']
-// //         },
-// //         boxShadow: {
-// //           '3xl': '#000000',
-// //         }
-// //       },
-// //     },
-// //     plugins: [],
-// //   }
-
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-//   content: ["./src/**/*.{html,js}"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         "dark-green": '#448F30',
-//         "light-green": '#DEFAD4',
-//         "grey-green": '#92ab89',
-//         "green-shaded": '#6cbd4f',
-//         "very-light-green": '#84bf71',
-//         "parrot-green": '#67FE33',
-//         "remove-red": '#eb4034',
-//         "card-green": '#F2FFED',
-//         "gray-color": '#cccccc'
-//       },
-//       fontFamily: {
-//         'clash': ['"Clash Display"', 'sans-serif']
-//       },
-//       boxShadow: {
-//         '3xl': '#000000',
-//       }
-//     },
-//   },
-//   plugins: [],
-// }
-
-/**
- * 
- /** 
-  
- 
- /**
-  @type {import('tailwindcss').Config} */
-module.exports = {
+ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -95,20 +31,20 @@ module.exports = {
         'custom': '32px',
       },
       textColor : {
-        "lightgray" : "lightgray:#808080",
+        "lightgray" : "#808080",
         'black': '#222222'
-
 
       },
       gap: {
-        "gap-12" : "gap:12px"
+        "12" : "12px"
       },
       height: {
-        "h-178": "height:178px"
+        "178": "178px"
       },
       width :{
-        "w-478": "width:478px",
-        "w-462": "width:462px"
+        "462": "462px",
+        "478": "478px",
+       
       },
     },
   },
