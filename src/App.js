4c2ee3d09b9e23e7912170a1f7a3e1f4f61c7d39
kavitha9894 <skavitha9894@gmail.com>
2024-06-03 18:38:51 +0530

    modefied code

--- src/App.js
+++ src/App.js
@@ -3,26 +3,30 @@ import 'tailwindcss/tailwind.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Things from './Component/Things';
 import Hii from './Component/whyus';
-import Glimb from './Component/Glimb'
-import GetStarted from "./Component/GetStarted";
+import Glimb from './Component/Glimb';
+import GetStarted from './Component/GetStarted';
 import Topbar from './Component/Topbar';
 import Home from './Component/Home';
+import AboutUs from'./Component/AboutUs';
+import Footer from './Component/Footer';
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
       <BrowserRouter>
-
-        <Routes>
+          
+          <Routes>
           <Route path="/Things" element={<Things />}></Route>
           <Route path="/Hii" element={<Hii/>}></Route>
           <Route path="/Glimb" element={<Glimb />}></Route>
           <Route path="/GetStarted" element={<GetStarted />}></Route>
           <Route path="/Home" element={<Home />}></Route>
-
-        </Routes>
+          <Route path="/AboutUs" element={<AboutUs/>}></Route>
+          
+          </Routes>
       </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
