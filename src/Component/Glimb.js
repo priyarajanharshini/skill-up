01c47e7d95c63a63e3377a8cd7db87973cf9d279
kavitha9894 <skavitha9894@gmail.com>
2024-06-08 18:00:27 +0530

    glimp changes

--- src/Component/Glimb.js
+++ src/Component/Glimb.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import web from '../Images/Rectangle 34625026 (3).svg';
 import course from '../Images/Rectangle 34625026 (4).svg';
-import group from '../Images/d05e837ab47e77798a11324647638b78.png';
+import group from '../Images/Rectangle 34625026 (4).svg';
 import clock from '../Images/clock.svg';
 import profile from '../Images/profile-2user.svg';
 import elipse1 from '../Images/Ellipse 6.svg';
@@ -18,12 +18,13 @@ function Glimb() {
   const cards = [
     {
       image: web,
-      title: 'HTML, CSS and Javascript for web developers',
+      title: 'HTML, CSS and Javascript for web developers ........',
       elipse: elipse1,
       instructor: 'Dan Scott',
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
     {
       image: course,
@@ -33,15 +34,17 @@ function Glimb() {
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
     {
       image: web,
-      title: 'HTML, CSS and Javascript for web developers',
+      title: 'HTML, CSS and Javascript for web developers ........',
       elipse: elipse1,
       instructor: 'Dan Scott',
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
     {
       image: course,
@@ -51,15 +54,17 @@ function Glimb() {
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
     {
       image: web,
-      title: 'HTML, CSS and Javascript for web developers',
+      title: 'HTML, CSS and Javascript for web developers ........',
       elipse: elipse1,
       instructor: 'Dan Scott',
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
     {
       image: course,
@@ -69,15 +74,17 @@ function Glimb() {
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
     {
       image: web,
-      title: 'HTML, CSS and Javascript for web developers',
+      title: 'HTML, CSS and Javascript for web developers ........',
       elipse: elipse1,
       instructor: 'Dan Scott',
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
     {
       image: course,
@@ -87,11 +94,11 @@ function Glimb() {
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
   ];
 
   const glimb = [
-  
     {
       image: web,
       title: 'HTML, CSS and Javascript for web developers',
@@ -100,24 +107,27 @@ function Glimb() {
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
     {
       image: course,
-      title: 'Introduction to Artificial Intelligence and Machine Learning',
+      title: 'Introduction to Artificial Intelligence and Machine...',
       elipse: elipse2,
       instructor: 'Jane Doe',
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
     {
       image: group,
-      title: ' Build Dynamic Websites with React',
+      title: 'Build Dynamic Websites with React ...............................',
       elipse: elipse3,
       instructor: 'Neha Basin',
       duration: '11 hr 30 min',
       profile: profile,
       viewers: '9,654',
+      rating: '4.8',
     },
   ];
 
@@ -126,28 +136,51 @@ function Glimb() {
       {showMore ? (
         <>
           <div className="mt-10">
-            <h3 className="text-center clash font-semibold text-5xl">
+            <h3 className="text-center font-clash font-semibold text-5xl">
               Our <span className="text-blue-600 clash">Popular Courses</span>
             </h3>
-            
           </div>
-          <div className="flex flex-wrap gap-8 mt-10 md:px-24 justify-center">
+          <div className=" px-4 flex flex-wrap  mt-10 md:px-16 justify-center">
             {cards.map((card, index) => (
               <div
                 key={index}
-                className=" p-4 shadow-xl rounded-lg overflow-hidden border-2 border-white sm:flex-100 md:flex-33 lg:flex-33"
-                style={{ backgroundColor: "#F6F6F6" }}
-              >
+                className="sm:flex-100 md:flex-35 lg:flex-48 "
+               
+              > 
+              <div className='shadow-xl rounded-3xl overflow-hidden border-2 border-white p-4 m-4 bg-custom-lightgray'>
+                <div className='relative'>
                 <div className="">
                   <img className="w-full h-full object-fill" src={card.image} alt="Card Image" />
                 </div>
-                <div className="mt-4">
-                  <h3 className="clash font-semibold text-2xl leading-7">{card.title}</h3>
-                  <div className="flex items-center mt-2">
-                    <img className="w-10 h-10 rounded-full" src={card.elipse} alt="Instructor" />
-                    <p className="ml-4 clash font-semibold text-sm leading-none">{card.instructor}</p>
+                <div 
+                className="w-88px h-12 rounded-xl   bg-white p-2 absolute bottom-0 right-0 mr-6 mb-6  flex items-center justify-center"
+              
+                >
+                  
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="orange"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="orange"
+                      className="w-6 h-6 mr-1"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 17.25l6.16 3.727-1.69-7.197 5.53-4.797-7.33-.637L12 1.5 9.33 8.346l-7.33.637 5.53 4.797-1.69 7.197L12 17.25z"
+                      />
+                    </svg>
+                    <span className='font-semibold text-xl'>{card.rating}</span>
                   </div>
-                  <div className="flex justify-between items-center mt-4">
+                </div>
+                <div className="mt-4">
+                  <h3 className="font-clash font-semibold text-2xl leading-7">{card.title}</h3>
+                  <div className="flex items-center mt-6">
+                    <img className="w-10 h-10 rounded-full" src={card.elipse} alt="Instructor" />
+                    <p className="ml-4 font-clash font-semibold text-xl leading-none">{card.instructor}</p>
+                  </div>
+                  <div className="flex justify-between items-center mt-6 mb-2">
                     <div className="flex items-center">
                       <img className="w-6 h-6" src={clock} alt="Clock" />
                       <span className="ml-2 font-normal lightgray outfit">{card.duration}</span>
@@ -157,6 +190,7 @@ function Glimb() {
                       <span className="ml-2 text-gray-500">{card.viewers}</span>
                     </div>
                   </div>
+                </div>
                 </div>
               </div>
             ))}
@@ -165,28 +199,52 @@ function Glimb() {
       ) : (
         <>
           <div className="mt-14">
-            <h3 className="text-center clash font-semibold text-5xl">
+            <h3 className="text-center font-clash font-semibold text-5xl">
               A glimpse on <span className="text-blue-600 block mt-3 clash">what we offer</span>
-
             </h3>
           </div>
-          <div className="flex flex-wrap gap-8 mt-10 md:px-24 justify-center">
+          <div className=" lg:px-16 flex flex-wrap mt-10 m-0">
             {glimb.map((card, index) => (
               <div
                 key={index}
-                className="shadow-xl rounded-lg overflow-hidden border-2 border-white  p-4  sm:flex-100 md:flex-33 lg:flex-25"
-                style={{ backgroundColor: "#F6F6F6" }}
+                className="sm:flex-100 md:flex-50 lg:flex-1 "
+                
               >
-                <div className="h-64 overflow-hidden">
-                  <img className="w-full h-full object-fill" src={card.image} alt="Card Image" />
+
+                <div className='shadow-xl rounded-3xl overflow-hidden border-2 border-white p-4 m-4 bg-custom-lightgray'>
+                <div className="relative">
+                  <div className="">
+                    <img
+                      className="w-full h-full object-fill"
+                      src={card.image}
+                      alt="Card Image"
+                    />
+                  </div>
+                  <div className="w-88px h-12 rounded-xl p-2 bg-white absolute bottom-0 right-0 mr-6 mb-6 flex items-center justify-center">
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="orange"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="orange"
+                      className="w-6 h-6 mr-1"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 17.25l6.16 3.727-1.69-7.197 5.53-4.797-7.33-.637L12 1.5 9.33 8.346l-7.33.637 5.53 4.797-1.69 7.197L12 17.25z"
+                      />
+                    </svg>
+                    <span className='font-semibold text-xl'>{card.rating}</span>
+                  </div>
                 </div>
                 <div className="mt-4">
-                  <h3 className="clash font-semibold text-2xl leading-7">{card.title}</h3>
-                  <div className="flex items-center mt-2">
+                  <h3 className="font-clash font-semibold text-2xl leading-7">{card.title}</h3>
+                  <div className="flex items-center mt-6">
                     <img className="w-10 h-10 rounded-full" src={card.elipse} alt="Instructor" />
-                    <p className="ml-4 clash font-semibold text-sm leading-none">{card.instructor}</p>
+                    <p className="ml-4 font-clash font-semibold text-xl leading-none">{card.instructor}</p>
                   </div>
-                  <div className="flex justify-between items-center mt-4">
+                  <div className="flex justify-between items-center mt-6 mb-4">
                     <div className="flex items-center">
                       <img className="w-6 h-6" src={clock} alt="Clock" />
                       <span className="ml-2 font-normal lightgray outfit">{card.duration}</span>
@@ -197,11 +255,13 @@ function Glimb() {
                     </div>
                   </div>
                 </div>
+                </div>
+               
               </div>
             ))}
           </div>
-          <div className="flex justify-center mt-4">
-            <button className="bg-blue-500 text-white py-5 px-9 rounded" onClick={handleViewAllClick}>
+          <div className="flex justify-center mt-12 mb-12">
+            <button className="bg-blue-500 text-white py-5 px-10 rounded-2xl" onClick={handleViewAllClick}>
               {showMore ? '' : 'View All'}
             </button>
           </div>
@@ -211,4 +271,4 @@ function Glimb() {
   );
 }
 
-export default Glimb; 
+export default Glimb;
