f620a4503beb1b84dc1123e443c7a59acc360948
keerthanaashokkumar <ajkeerthana1997@gmail.com>
2024-06-11 12:22:28 +0530

    getstarted corrections

--- src/Component/GetStarted.js
+++ src/Component/GetStarted.js
@@ -7,15 +7,15 @@ const GetStarted = () => {
 
   return (
     <>
-      <div className="sm:pt-75px md:pt-75px lg:pt-120px flex flex-col relative w-11/12 lg:w-10/12 md:w-9/12 sm:w-8/12 bg-black rounded-40px text-white overflow-hidden mx-auto mt-50px pt-75px">
+      <div className="sm:pt-75px md:pt-14 lg:pt-120px pb-75px sm:pb-75px md:pb-14 lg:pb-120px flex flex-col relative w-11/12 lg:w-10/12 md:w-9/12 sm:w-8/12 bg-black rounded-40px text-white overflow-hidden mx-auto mt-50px mb-50px pt-75px">
         <div className="relative flex flex-col items-center">
-          <h1 className="font-clash font-semibold text-2xl sm:text-2xl md:text-3xl lg:text-5xl xl:text-6xl text-center text-white">
-            Make the most of your<br />Online learning experience
+          <h1 className="font-clash font-semibold text-2xl sm:text-2xl md:text-3xl lg:text-6xl xl:text-6xl text-center text-white leading-tight px-8 lg:px-40">
+            Make the most of your Online learning experience
           </h1>
-          <p className="font-outfit font-light leading-8 text-lg text-center text-white  mt-5 px-6">
-            Our Online Learning Resource Centre has tips, tricks and<br />inspiring stories to help you learn while staying home.
+          <p className="font-outfit font-light leading-8 text-lg text-center text-white mt-5 px-6 sm:px-12 md:px-20 lg:px-80">
+            Our Online Learning Resource Centre has tips, tricks and inspiring stories to help you learn while staying home.
           </p>
-          <button className="px-6 sm:px-8 md:px-60px py-3 sm:py-4 md:py-5 w-60 rounded-2xl bg-blue-500 hover:bg-blue-700 mt-5 mb-120px">
+          <button className="px-6 sm:px-8 md:px-60px py-3 sm:py-4 md:py-5 w-60 rounded-2xl bg-blue-500 hover:bg-blue-700 mt-8">
             Get Started
           </button>
         </div>
