ea0e5c2ea11341c27fc45f0e46b239943a470169
keerthanaashokkumar <ajkeerthana1997@gmail.com>
2024-06-12 17:07:45 +0530

    statisfy font added

--- src/Component/AboutUs.js
+++ src/Component/AboutUs.js
@@ -6,23 +6,56 @@ import playIcon from '../Images/PlayIcon.svg'
 const AboutUs = () => {
   return (
     <>
-    <div className="flex flex-col items-center gap-6 absolute w-full h-[647px] top-[184px] overflow-hidden">
-      <div className="flex flex-col items-center">
-        <h1 className="font-clash text-4xl sm:text-5xl font-semibold leading-[48px] sm:leading-[67.2px] text-center w-[280px] sm:w-[831px] h-[auto] sm:h-[67px] gap-0">
-          About <span className="text-sky-blue">us</span>
-        </h1>
-        <div className="font-outfit text-lg sm:text-2xl font-light leading-6 sm:leading-8 text-center  text-black w-[280px] sm:w-[855px] h-[auto] sm:h-[32px] gap-0 opacity-100">
-          Our mission is to accelerate your career and help you explore new technologies.
+      <div className="flex flex-col relative items-center gap-6 mt-14 overflow-hidden">
+        <div className="flex flex-col items-center">
+          <h1 className="font-clash text-4xl sm:text-5xl font-semibold leading-48px sm:leading-67.2px text-center w-280px sm:w-831px h-auto sm:h-67px gap-0">
+            About <span className="text-sky-blue">us</span>
+          </h1>
+          <p className="font-outfit text-lg sm:text-2xl w-full font-light leading-6 sm:leading-8 text-center p-1 text-black xz:px-120px sm:px-120px md:px-20 lg:px-40">
+            Our mission is to accelerate your career and help you explore new technologies.
+          </p>
         </div>
+        {/* <div className="relative  container w-[380px] sm:w-[540px] md:w-[720px] lg:w-[1080px] h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] mx-auto"> */}
+        <div className=" container relative w-96 lg:w-10/12 md:w-9/12 sm:w-4/5 rounded-40px overflow-hidden mx-auto p-2">
+          <img className="w-full h-full rounded-2xl	 sm:rounded-3xl md:rounded-28px lg:rounded-32px object-cover" src={aboutusvideo} alt="Outer" />
+          <div className="absolute inset-0 flex items-center justify-center">
+            <img className="object-contain p-4 w-28 h-28 xs:w-28 xs:h-28 sm:w-36 sm:h-36 md:w-44 md:h-36  lg:w-48 lg:h-44" src={playIcon} alt="Play Icon" />
+          </div>
+          </div>
       </div>
-      <div className="relative  container w-[380px] sm:w-[540px] md:w-[720px] lg:w-[1080px] h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] mx-auto">
-        <img className="w-full h-full rounded-[16px] sm:rounded-[24px] md:rounded-[28px] lg:rounded-[32px] object-cover" src={aboutusvideo} alt="Outer Image" />
-        <div className="absolute inset-0 flex items-center justify-center">
-        <img className="w-50 h-50 object-contain p-4" src={playIcon} alt="Play Icon" />      
+      <div class="container mx-auto mt-16 sm:mt-24 md:mt-24 lg:mt-24 px-4 sm:px-6 md:px-8">
+        <div class="flex flex-wrap justify-start justify-center -mx-2">
+          <div class="w-full flex flex-wrap justify-start -mx-2 mb-10">
+            <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-2 mb-8">
+
+              <div class="border border-gray-100 shadow-custom-box rounded-3xl p-0.5">
+                <div class="bg-[#e7f4ff] p-10 rounded-3xl mx-auto h-40">
+                  <p class="font-clash text-5xl font-semibold leading-72 text-left">2020</p>
+                  <p class="font-clash text-base font-medium leading-6 text-left text-gray-400  pt-2 ">founded</p>
+                </div>
+              </div>
+            </div>
+            <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-2 mb-8">
+              <div class="border border-white-100 shadow-custom-box rounded-3xl p-0.5">
+                <div class="bg-[#e7f4ff] p-10 rounded-3xl mx-auto text-center h-40">
+                  <p class="font-clash text-5xl font-semibold leading-72 text-left">15,630+</p>
+                  <p class="font-clash text-base font-medium leading-6 text-left text-gray-400  pt-2">satisfied students</p>
+                </div>
+              </div>
+            </div>
+            <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-2 mb-8">
+              <div class="border border-white-100 shadow-custom-box rounded-3xl p-0.5">
+                <div class="bg-[#e7f4ff] p-10 rounded-3xl mx-auto text-center h-40">
+                  <p class="font-clash text-5xl font-semibold leading-72 text-left">50+</p>
+                  <p class="font-clash text-base font-medium leading-6 text-left text-gray-400 pt-2">personlized courses</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+
       </div>
-    </div>
-</>
+    </>
   )
 
 }
