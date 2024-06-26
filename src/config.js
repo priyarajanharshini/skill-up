b81c29599c2a1074be3dd6d0a970fa687a98fea5
keerthanaashokkumar <ajkeerthana1997@gmail.com>
2024-06-26 10:04:31 +0530

    config url and glimb url correction

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const BASE_URL = 'http://localhost:2000';
-
+// export const BASE_URL='http://13.201.159.83:2000'
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
